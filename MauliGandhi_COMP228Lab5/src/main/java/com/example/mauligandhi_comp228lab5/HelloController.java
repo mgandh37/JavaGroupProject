e670794e3086a6bd2ed076e77f75e80f657e2e3e
SUilhl3 <ilagang@my.centennialcollege.ca>
2024-12-02 19:33:16 -0500

    Changed the UI box
    - Changed the UI nothing works yet but its better then everything breaking for now

--- MauliGandhi_COMP228Lab5/src/main/java/com/example/mauligandhi_comp228lab5/HelloController.java
+++ MauliGandhi_COMP228Lab5/src/main/java/com/example/mauligandhi_comp228lab5/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.mauligandhi_comp228lab5;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.sql.SQLException;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController
+{
+    public void addButtonAction (ActionEvent actionEvent) throws SQLException
+    {
+
+    }
+
+    public void updateButtonAction(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void deleteButtonAction(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void queryButtonAction(ActionEvent actionEvent)
+    {
+
     }
 }
